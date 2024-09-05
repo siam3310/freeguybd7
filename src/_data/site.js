6742789c70202f87b6b2593bdc6a36e1826eb493
Mahamudun nabi siam <eaenkes100@nic.edu.pl>
2024-09-05 14:57:45 +0600

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: 'Vredeburg',
-  description: 'Eleventy starter project to create a blog',
-  keywords: ['eleventy', 'template', 'simple', 'clean'],
-  url: 'https://vredeburg.netlify.app', // your site url without trailing slash
+  title: 'FreeGuyBD',
+  description: 'Nothing is PREMIUM',
+  keywords: ['Mahamudun Nabi Siam', 'Cinebuzzbd', 'FreeguyBD', 'freeguybd'],
+  url: 'https://freeguybd7.netlify.app', // your site url without trailing slash
   paginate: 6 // how many posts you want to show for each page
   // uncomment the next line if you want to add disqus to your site
   // disqusShortname: "your-shortname"
